0f925150e29adfaf6abd90b7854b0de772230f74
dong2810 <53572317+dong2810@users.noreply.github.com>
2023-10-13 14:57:07 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,35 +1,19 @@
-// swift-tools-version:5.5
-
+// swift-tools-version:5.7.1
 import PackageDescription
 
 let package = Package(
-    name: "BZipCompression",
-    platforms: [
-        .iOS(.v13),
-        .macOS(.v10_15),
-        .watchOS(.v6),
-        .tvOS(.v13),
-    ],
-    products: [
-        .library(
-            name: "BZipCompression",
-            targets: ["BZipCompression"]
-        ),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/dong2810/BZipCompression.git", from: "0.4.0"),
-    ],
-    targets: [
-        .target(
-            name: "BZipCompression",
-            dependencies: [
-                "BZipCompression",
-            ],
-            path: "Code"
-        ),
-        .testTarget(
-            name: "BZipCompressionTests",
-            dependencies: ["BZipCompression"]
-        ),
-    ]
+	name: "BZipCompression",
+	defaultLocalization: "en",
+	platforms: [.iOS(.v13)],
+	products: [
+		.library(
+			name: "BZipCompression",
+			targets: ["BZipCompression"]),
+	],
+	targets: [
+		.target(
+			name: "BZipCompression",
+			path: "Code",
+			publicHeadersPath: ""),
+	]
 )
